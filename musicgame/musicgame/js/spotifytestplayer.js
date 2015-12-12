39752e1c6959a99dc8e2f5a0b97c56daec438ad5
ryanpiv <ryanpiveteau@gmail.com>
2015-12-11 19:31:24 -0500

    created vs solution and implemented example app

---
+++ musicgame/musicgame/js/spotifytestplayer.js
@@ -0,0 +1,58 @@
+﻿// find template and compile it
+var templateSource = $('#results-template').html(),
+    template = Handlebars.compile(templateSource),
+    resultsPlaceholder = $('#results'),
+    playingCssClass = 'playing',
+    audioObject = null;
+
+var fetchTracks = function (albumId, callback) {
+    $.ajax({
+        url: 'https://api.spotify.com/v1/albums/' + albumId,
+        success: function (response) {
+            callback(response);
+        }
+    });
+};
+
+var searchAlbums = function (query) {
+    $.ajax({
+        url: 'https://api.spotify.com/v1/search',
+        data: {
+            q: query,
+            type: 'album'
+        },
+        success: function (response) {
+            console.log(response);
+            resultsPlaceholder.html(template(response));
+        }
+    });
+};
+
+results.addEventListener('click', function (e) {
+    var target = e.target;
+    if (target !== null && target.classList.contains('cover')) {
+        if (target.classList.contains(playingCssClass)) {
+            audioObject.pause();
+        } else {
+            if (audioObject) {
+                audioObject.pause();
+            }
+            fetchTracks(target.getAttribute('data-album-id'), function (data) {
+                audioObject = new Audio(data.tracks.items[0].preview_url);
+                audioObject.play();
+                target.classList.add(playingCssClass);
+                audioObject.addEventListener('ended', function () {
+                    target.classList.remove(playingCssClass);
+                });
+                audioObject.addEventListener('pause', function () {
+                    target.classList.remove(playingCssClass);
+                });
+            });
+        }
+    }
+});
+
+document.getElementById('search-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    searchAlbums($('#artist').val(), $('#song').val(), $('#album').val());
+}, false);
